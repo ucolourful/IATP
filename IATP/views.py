51d52fd270dbff297b21a16312ed7d59c54b5ad7
liming <506231730@qq.com>
2018-12-02 23:45:23 +0800

    【提交原因】：建立登陆页 【下次提交】：使用frameset

---
+++ IATP/views.py
@@ -0,0 +1,21 @@
+import json
+
+from django.contrib import auth
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def login(request):
+    if request.POST:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            request.session['user'] = username
+            return HttpResponse(json.dumps({"status": 0, "msg": "登录成功，即将跳转到首页..."}),
+                                content_type="application/json")
+        else:
+            return HttpResponse(json.dumps({"status": 1, "msg": "登录失败，用户名或密码错误"}),
+                                content_type="application/json")
+    return render(request, "login.html")
