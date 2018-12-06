33e564cc5bcfc189f2f9b2378bbc9c6d0fcba598
liming <506231730@qq.com>
2018-12-06 22:46:12 +0800

    【提交原因】：home页顶部和侧边栏设计完毕 【下次提交】：home页主体内容部分

--- static/js/iatp-home.js
+++ static/js/iatp-home.js
@@ -1,3 +1,21 @@
-//IATP 登录页 JS
-layui.use(['element', 'form'], function () {
+//IATP 首页 JS
+layui.use(['element', 'form', 'jquery', 'layer'], function () {
+    let $ = layui.jquery;
+    let layer = layui.layer;
+
+    $("#user-menu dd").on("click",function () {
+       $(this).removeClass();
+       if ( this.id === "user-info" ){
+           layer.msg("基本资料");
+       }
+       if ( this.id === "user-set" ){
+           layer.msg("个人设置")
+       }
+    });
+
+    // 侧边导航栏监听
+    $("#side-list li").on("click", function () {
+        layer.msg($(this).attr("data-value"));
+        $("#body-content").html($(this).attr("data-value"));
+    });
 });
